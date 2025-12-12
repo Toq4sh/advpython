e6d155ad843a69f82643ca55a3aebc4e4531063e
Toktar <serikovt981@gmail.com>
2025-12-12 18:18:28 +0500

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,7 @@
+#task1
+first_name, last_name = input("ur full name: ").split()
+age=int(input("how old are you? "))
+pnumb=int(input("ur nuber? "))
+print(first_name, last_name)
+print(age)
+print(pnumb)
